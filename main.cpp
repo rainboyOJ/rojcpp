cde49839eb600d339d1360a1d09ff0db53f54323
rainboy <rainboylvx@qq.com>
2022-03-04 15:27:23 +0800

    helloworld

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 #include "http_server.hpp"
 #include "websocket_manager.h"
 
+using namespace rojcpp;
+rojcpp::http_server_ Server;
+
+
 int main(){
-    
+    Server.set_http_handler<GET>("/helloworld",[](request & req,response & res){
+            res.set_status_and_content(status_type::ok,"hello world,this rojcpp Server",req_content_type::string);
+    });
+
+    Server.run();
     return 0;
 }
